2db715aca959bbaeed4d249fa3e4460e22dba53b
m4zyz <65948333+m4zyz@users.noreply.github.com>
2021-07-05 11:05:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'github'){
+    if(command === 'backdoor'){
 
-        client.commands.get('github').execute(message, args);
+        client.commands.get('backdoor').execute(message, args);
     }
 });
 
