fb562f17d39f7cb446d89a3f71db62fd8bd157af
m4zyz <65948333+m4zyz@users.noreply.github.com>
2021-07-05 21:27:56 +0200

    Update backdoor.js

--- commands/backdoor.js
+++ commands/backdoor.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'github',
-    description: "this send m4zyz's github!",
+    name: 'backdoor',
+    description: "backdoor",
     execute(message, args){
 
         if(message.member.roles.cache.has('Enter the rank you want')){
