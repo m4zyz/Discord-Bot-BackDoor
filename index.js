00ec88d3e50b46f9c5842d247d5d913246b59bf3
m4zyz <65948333+m4zyz@users.noreply.github.com>
2021-07-05 10:59:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '.';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(files => files.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+
+client.once('ready', () => {
+    console.log(`Successfully logged in as ${client.user.username}!`);
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+
+        client.commands.get('ping').execute(message, args);
+
+    } else if (command === 'github'){
+        
+        client.commands.get('github').execute(message, args);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('ODUzMzQzNzM2NDA2NzM2OTI2.YMUASQ.nTHnbGSoOs2iVb2Mkk203PNKhhU');
